f2bc6a1568ff6b8358d07725adccccd5f66733dd
tatkien <tatkien06082005@gmail.com>
2025-09-01 17:25:50 +0700

    Refactor dataset download script to use shutil for copying and ensure destination directory creation

--- download_dataset.py
+++ download_dataset.py
@@ -1,11 +1,15 @@
 import kagglehub
 import os
+import shutil
 
 # Download latest version
 path = kagglehub.dataset_download("aryan208/financial-transactions-dataset-for-fraud-detection")
 print("Downloaded to:", path)
-os.mkdir("dataset", exist_ok=True)
-dest = os.getcwd() + "/dataset/"
 
-# Move to desired location
-os.rename(path, dest)
+# Create destination directory if it doesn't exist
+dest = os.getcwd() + "/dataset/"
+os.makedirs(dest, exist_ok=True)
+
+
+# Copy to desired location
+shutil.copytree(path, dest, dirs_exist_ok=True)
